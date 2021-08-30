4ea3ec6ca54f1160a2afd80529b52232fa0893d0
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-08-30 22:52:48 +0200

    Scrap more job indicators

--- main.py
+++ main.py
@@ -20,9 +20,29 @@ response.raise_for_status()
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-global_content = soup.select(selector=".jobCard_mainContent")
+global_content = soup.select(selector=".job_seen_beacon")
 print(global_content)
 
 for content in global_content:
-    test = content.h2.getText().replace("nouveau", "")
-    print(test)
+    job_title = content.h2.getText().replace("nouveau", "")
+    job_company = content.find("span", "companyName").getText()
+    job_location = content.find("div", "companyLocation").getText()
+
+    try:
+        job_salary = content.find("span", "salary-snippet").getText()
+    except AttributeError:
+        job_salary = "Absence de données"
+
+    try:
+        post_date = (n for n in content.find("span", "date").getText() if n.isdigit())  # ne fonctionne pas
+    except AttributeError:
+        post_date = 0
+
+    job_summary = content.find("li").getText()
+    print(job_title)
+    print(job_company)
+    print(job_location)
+    print(job_salary)
+    print(post_date)
+    print(type(post_date))
+    print(job_summary)
