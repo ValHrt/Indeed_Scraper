33cc0e3770cf7c275c8d7b8895f107bf95868bab
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-09-01 14:20:33 +0200

    OOP / Find next page

--- main.py
+++ main.py
@@ -1,62 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-import datetime as dt
+from scraper import Scraper
 
-endpoint = "https://fr.indeed.com/jobs"
-
-today = dt.datetime.today()
 
 job_name = input("Intitulé du poste : ")
 job_location = input("Lieu du poste : ")
 job_distance = input("Combien de kilomètres maximum du lieu indiqué (par tranche de 25km) : ")
 job_type = input("Type de poste recherché (internship, permanent, ...) : ")
 
-parameters = {
-    "q": job_name,
-    "l": job_location,
-    "radius": job_distance,
-    "jt": job_type,
-}
+scraper = Scraper(job_name, job_location, job_distance, job_type)
 
-response = requests.get(endpoint, params=parameters)
-response.raise_for_status()
-final_url = response.url
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-global_content = soup.select("a[data-jk]")
-# print(global_content)
+global_content = scraper.global_content
 
 for content in global_content:
-    job_title = content.h2.getText().replace("nouveau", "")
-    job_company = content.find("span", "companyName").getText()
-    job_location = content.find("div", "companyLocation").getText()
-    job_summary = content.find("div", "job-snippet").getText().strip()
+    data = scraper.get_record(content, scraper.today, scraper.final_url)
+    print(data)
 
-    try:
-        job_salary = content.find("span", "salary-snippet").getText()
-    except AttributeError:
-        job_salary = "Absence de données"
-
-    try:
-        post_date = int(re.findall("\\d+", content.find("span", "date").getText())[0])
-        if post_date < 30:
-            post_date = (today - dt.timedelta(days=post_date)).strftime("%Y-%m-%d")
-        else:
-            post_date = "Posté il y a plus de 30 jours"
-    except IndexError:
-        post_date = today.strftime("%Y-%m-%d")
-
-    try:
-        job_url = final_url + "&advn=" + content.get("data-empn") + "&vjk=" + content.get("data-jk")
-    except TypeError:
-        job_url = final_url + "&vjk=" + content.get("data-jk")
-
-    print(job_title)
-    print(job_company)
-    print(job_location)
-    print(job_salary)
-    print(post_date)
-    print(job_summary)
-    print(job_url)
+test = scraper.get_next_page(scraper.soup)
+print(test)
--- scraper.py
+++ scraper.py
@@ -6,8 +6,9 @@ import datetime as dt
 
 class Scraper:
 
-    def __init__(self, name: str, location: str, distance: int, j_type: str):
-        self.endpoint = "https://fr.indeed.com/jobs"  # Partie à modifier pour pouvoir changer le pays
+    def __init__(self, name: str, location: str, distance: str, j_type: str):
+        self.today = dt.datetime.today()
+        self.endpoint = "https://fr.indeed.com/jobs"  # Partie à inclure dans l'UI pour choix du pays
         self.name = name
         self.location = location
         self.distance = distance
@@ -18,6 +19,45 @@ class Scraper:
                 "radius": self.distance,
                 "jt": self.j_type,
         }
+        self.response = requests.get(self.endpoint, self.parameters)
+        self.final_url = self.response.url
+        self.soup = self.scrape_html(self.response.text)[0]
+        self.global_content = self.scrape_html(self.response.text)[1]
 
-    def url_request(self, endpoint, parameters):
-        pass
+    @staticmethod
+    def get_record(content, today, final_url):
+        job_title = content.h2.getText().replace("nouveau", "")
+        job_company = content.find("span", "companyName").getText()
+        job_location = content.find("div", "companyLocation").getText()
+        job_summary = content.find("div", "job-snippet").getText().strip().replace("\n", " ")
+
+        try:
+            job_salary = content.find("span", "salary-snippet").getText().replace("\xa0", "")
+        except AttributeError:
+            job_salary = "Absence de données"
+
+        try:
+            post_date = int(re.findall("\\d+", content.find("span", "date").getText())[0])
+            if post_date < 30:
+                post_date = (today - dt.timedelta(days=post_date)).strftime("%Y-%m-%d")
+            else:
+                post_date = "Posté il y a plus de 30 jours"
+        except IndexError:
+            post_date = today.strftime("%Y-%m-%d")
+
+        try:
+            job_url = final_url + "&advn=" + content.get("data-empn") + "&vjk=" + content.get("data-jk")
+        except TypeError:
+            job_url = final_url + "&vjk=" + content.get("data-jk")
+        record = (job_title, job_company, job_location, job_salary, post_date, job_summary, job_url)
+        return record
+
+    @staticmethod
+    def scrape_html(html_text: str):
+        soup = BeautifulSoup(html_text, "html.parser")
+        global_content = soup.select("a[data-jk]")
+        return soup, global_content
+
+    def get_next_page(self, soup):  # Méthode à corriger (ne fonctionne pas)
+        self.final_url = self.endpoint + soup.select('a[aria-label="suivant"]')
+        return self.final_url
