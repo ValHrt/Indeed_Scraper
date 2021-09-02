2bcbdd7c5ddd329d83360e207fcfb16816e461dd
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-09-02 19:35:12 +0200

    Add datasave module

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from scraper import Scraper
+from datasave import DataSave
 
 
 job_name = input("Intitulé du poste : ")
@@ -9,30 +10,35 @@ job_distance = input("Combien de kilomètres maximum du lieu indiqué (par tranc
 job_type = input("Type de poste recherché (internship, permanent, ...) : ")
 
 scrap = True
-i = 1  # Pour phase de test / A supprimer ensuite / Voir pour faire sous forme d'inputs avec le nombre de résultats voulu
 
 scraper = Scraper(job_name, job_location, job_distance, job_type)
 global_content = scraper.global_content
 print(scraper.final_url)
+
+datasave = DataSave(scraper.today.strftime("%Y-%m-%d"), job_type.title())
+
+records = []
 
 while scrap:
 
     for content in global_content:
         data = scraper.get_record(content, scraper.today, scraper.final_url)
         print(data)
+        records.append(data)
 
+    scraper.page_number += 1
     next_page = scraper.get_next_page(scraper.soup)
 
-    if next_page == "Fin du scrapping" or i > 3:
-        print(next_page)
+    if next_page == "Fin du scrapping":
         scrap = False
 
     else:
         scraper.response = requests.get(next_page)
         scraper.response.raise_for_status()
         scraper.final_url = scraper.response.url
-        # print(scraper.final_url)
+        print(scraper.final_url)  # Fait ressortir un problème dans la redirection des liens (perte de la donnée job_name dans l'url)
         scraper.soup = scraper.scrape_html(scraper.response.text)[0]
         global_content = scraper.scrape_html(scraper.response.text)[1]
         time.sleep(5)
-        i += 1
+
+datasave.save_to_csv(job_location.title(), records)
